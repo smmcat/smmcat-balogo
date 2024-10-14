213aca88f6caff8dd354e01079f8c1080d468136
smmcat <1365116009@qq.com>
2024-10-14 09:22:29 +0800

    modified:   package.json 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
-import { Context, Schema } from 'koishi'
-
+import { Context, Schema, h } from 'koishi'
 export const name = 'smmcat-balogo'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  const baLogoTool = {
+    async setImg(top: string, bottom: string) {
+      if (!bottom) {
+        bottom = top.slice(Math.floor(top.length / 2))
+        top = top.slice(0, Math.floor(top.length / 2))
+      }
+      return h.image(`https://oiapi.net/API/BlueArchive?startText=${encodeURIComponent(top)}&endText=${encodeURIComponent(bottom)}`)
+    }
+  }
+  ctx
+    .command('ba生图 <text:text>')
+    .action(async ({ session }, text) => {
+      if (!text) {
+        await session.send("请输入你想生成的字符。符号左右文字用空格隔开 (10秒内输入)")
+        text = await session.prompt(10000)
+        if (!text) {
+          return
+        }
+      }
+      const res = h.select(text, 'text').map(item => item.attrs.content).join('').split(/\s+/).filter(str => str !== "").slice(0, 2);
+      return baLogoTool.setImg(res[0], res[1])
+    })
 }
